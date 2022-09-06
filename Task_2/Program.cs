ec3de92fbd83b7abc40efecf5e254682438ccc6b
Elvira <eliasufa@yahoo.com>
2022-09-06 23:42:08 +0500

    Задача 2 готова

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -4,7 +4,18 @@ double k1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите координаты прямой B через Enter: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 double k2 = Convert.ToDouble(Console.ReadLine());
-double intersection_x = (b2 - b1) / (k1 - k2);
-double intersection_y = k2 * intersection_x + b2;
-Console.WriteLine(intersection_x);
-Console.WriteLine(intersection_y);
+if(b1 * k2 - b2 * k1 == 0)
+{
+    if(b1 * k2 == k1 * b2)
+    {
+        Console.WriteLine("Заданные прямые совпадают.");
+        return;
+    }
+    Console.WriteLine("Заданные прямые параллельны.");
+}
+else
+{
+    double cross_x = (b2 - b1) / (k1 - k2);
+    double cross_y = k2 * cross_x + b2;
+    Console.WriteLine($"Координаты точки пересечения заданных прямых: х = {cross_x}, у = {cross_y}");
+}
