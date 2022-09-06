f16eea96c7147d5eb88ed408b3b7c0b09a581291
Elvira <eliasufa@yahoo.com>
2022-09-06 18:45:41 +0500

    Задание 1 выполнено с помаркой

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿New:
+Console.WriteLine("Введите любое натуральное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if(M <= 0)
+{
+    Console.WriteLine("Введенное число не натуральное!");
+    goto New;
+}
+Console.WriteLine($"Введите любые {M} чисел через Enter: ");
+for(int i = 0; i < M; i++)
+{
+    string s = Console.ReadLine();
+    double m = Convert.ToDouble(s);
+    if(m > 0)
+    {
+        count += 1;
+    }
+}
+Console.WriteLine($"Количество введенных положительных чисел = {count}"); 
